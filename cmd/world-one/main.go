540421fcfd42341331c4ffef6def91dcc6828d85
Sawyer Watts <sawyer.watts@gmail.com>
2024-12-09 19:28:58 -0800

    appease staticcheck

--- cmd/world-one/main.go
+++ cmd/world-one/main.go
@@ -112,3 +112,52 @@ func main() {
 
 	os.Exit(exitCode)
 }
+
+type mainSettings struct {
+	TimeZone               string
+	Addr                   string
+	ReadTimeoutSec         int
+	WriteTimeoutSec        int
+	IdleTimeoutSec         int
+	MaxGracefulShutdownSec int
+	SlogIncludeSource      bool
+	DBConnectionString     string `mapstructure:"PGURL"`
+}
+
+func newMainSettings() *mainSettings {
+	return &mainSettings{
+		TimeZone:               "GMT",
+		Addr:                   "",
+		ReadTimeoutSec:         30,
+		WriteTimeoutSec:        90,
+		IdleTimeoutSec:         120,
+		MaxGracefulShutdownSec: 5,
+		SlogIncludeSource:      false,
+		DBConnectionString:     "",
+	}
+}
+
+func (s *mainSettings) Validate() error {
+	if s.TimeZone == "" {
+		return errors.New("setting TimeZone is not initialized")
+	}
+	if s.Addr == "" {
+		return errors.New("setting Addr is not initialized")
+	}
+	if s.ReadTimeoutSec < 1 {
+		return errors.New("setting ReadTimeoutSec is not positive")
+	}
+	if s.WriteTimeoutSec < 1 {
+		return errors.New("setting WriteTimeoutSec is not positive")
+	}
+	if s.IdleTimeoutSec < 1 {
+		return errors.New("setting IdleTimeoutSec is not positive")
+	}
+	if s.MaxGracefulShutdownSec < 1 {
+		return errors.New("setting MaxGracefulShutdownSec is not positive")
+	}
+	if s.DBConnectionString == "" {
+		return errors.New("setting DBConnectionString is not initialized")
+	}
+	return nil
+}
